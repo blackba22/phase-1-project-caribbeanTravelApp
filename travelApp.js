dfa343bde0d39e5730e9854972ca13cf61c4aa62
+ <+>
2023-05-12 18:09:30 -0400

    added dropdown button to html

--- travelApp.js
+++ travelApp.js
@@ -1,6 +1,14 @@
 console.log("travel app works")
 
-fetch("http://localhost:3000/caribbeanCountries").then (res => res.json()).then(json => console.log(json))
+fetch("http://localhost:3000/caribbeanCountries")
+.then (res => res.json())
+.then((caribbeanCountries) => {
+    const container = document.querySelector("form#carribeanCountries");
+    caribbeanCountries.forEach(caribbeanCountry => {
 
+    })
+});
 
+//create a dropdown button where you can select flights or hotels
+//create a like button that you can click and it can highlight or not
 
