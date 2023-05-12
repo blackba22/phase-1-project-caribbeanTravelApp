7b49ce285f72d93966517bef6d8388ad73eae0e7
+ <+>
2023-05-12 19:55:05 -0400

    updated db.json with new images

--- travelApp.js
+++ travelApp.js
@@ -9,4 +9,18 @@ function createCardElement(places) {
     let card = document.createElement("div")
     card.classList.add("card") 
     console.log(places)
+    let legend = document.createElement("legend");
+legend.textContent = `Travel to Beautiful ${places}`
+let description = document.createElement("p");
+p.textContent = caribbeanCountries.description
+let image = document.createElement("img");
+image.src = caribbeanCountries.image
+let likes = document.createElement("p");
+p.textContent = "Is this the vacation of your dreams?? Let us know by clicking like below!"
+let button = document.createElement("button");
+button.classList.add("like-button")
+button.textContent = "Like ❤️"
+
+card.appendChild(legend, p, img, p, button)
+
 }
